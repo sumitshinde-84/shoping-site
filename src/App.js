6c7a447882d641e8923ed58183c40f722a9fd9e7
sumitshinde-84 <shindesumit217@gmail.com>
2023-04-29 21:57:18 +0530

    add imageGrid component

--- src/App.js
+++ src/App.js
@@ -5,22 +5,43 @@ import Navbar from "./components/navBar";
 import Intro from "./components/intro";
 import Textreveal from "./components/textreveal";
 import ImgGrid from "./components/imgGrid";
+import gsap from "gsap";
+import ScrollToPlugin from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollToPlugin);
+
 const App = () => {
-  const [pageLoadStatus,setPageLoadStatus]  = useState(false)
+  const [pageLoadStatus, setPageLoadStatus] = useState(false);
   const main = useRef(null);
 
-  const getPageLoadStatus = (status)=>{
-    setPageLoadStatus(status)
-  } 
+  const getPageLoadStatus = (status) => {
+    setPageLoadStatus(status);
+  };
 
+  const handleSmoothScroll = () => {
+    gsap.to(window, {
+      duration: 1, // Adjust the duration for smooth scrolling
+      scrollTo: { y: 0 }, // Scroll to the top of the page
+      ease: "power2.out", // Adjust the easing for smooth effect
+    });
+  };
+
+  useEffect(() => {
+    const bodyElement = document.querySelector("body");
+    bodyElement.addEventListener("scroll", handleSmoothScroll);
+
+    return () => {
+      bodyElement.removeEventListener("scroll", handleSmoothScroll);
+    };
+  }, []);
 
   return (
     <div ref={main} className="main-container">
       <Navbar />
-      <Intro  status={pageLoadStatus} />
-      <Loader getPageLoadStatus={getPageLoadStatus} />
-      <Textreveal/>
-      <ImgGrid/>
+      <Intro status={pageLoadStatus} />
+      {/* <Loader getPageLoadStatus={getPageLoadStatus} /> */}
+      <Textreveal />
+      <ImgGrid />
     </div>
   );
 };
--- src/components/imgGrid.jsx
+++ src/components/imgGrid.jsx
@@ -1,23 +1,57 @@
-import ImgGridImg from "../images/man.jpg"
-import "./style/imgGrid.css"
+import { useEffect } from "react";
+import ImgGridImg from "../images/man.jpg";
+import horizontal from "../images/horizontal.png";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import "./style/imgGrid.css";
+import { useRef } from "react";
 
 const ImgGrid = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  const imgGrid = useRef(null);
 
+  useEffect(() => {
+    const scrollBarSmooth = gsap.timeline({
+      scrollTrigger: {
+        trigger: imgGrid.current,
+        start: "top center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
 
-    return (
-        <div className="ImgGrid">
-            <div className="textcontent">
-            <h1>Welcome to our brand new store</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, beatae aliquid, consectetur repellat a rem necessitatibus, voluptatem laudantium architecto impedit asperiores? Nostrum ducimus ipsam a numquam omnis iste nam ut?
-            </p>
-            </div>
-            <div className="imgGridSect">
-                <img src={ImgGridImg} alt="imgGrid" />
-                <div className="background-of-Img"></div>
-            </div>
-        </div>
-    )
-}
+    scrollBarSmooth.to(document.body, {
+        color:"#f3f1f0",
+        backgroundColor: "#121212", 
+        // Change this to the desired new background color
+      ease: "power2.out", // Adjust the easing for a smooth effect
+    });
 
-export default ImgGrid
+    scrollBarSmooth.to(document.body, {
+        color:"#121212",
+        backgroundColor: "#f3f1f0",// Change this to the original background color
+      ease: "power2.out", // Adjust the easing for a smooth effect
+    });
+  }, []);
+
+  return (
+    <div ref={imgGrid} className="ImgGrid">
+      <div className="textcontent">
+        <h1>Welcome to our brand new store</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          beatae aliquid, consectetur repellat a rem necessitatibus, voluptatem
+          laudantium architecto impedit asperiores? Nostrum ducimus ipsam a
+          numquam omnis iste nam ut?
+        </p>
+      </div>
+      <div className="imgGridSect">
+        <img className="verticle" src={ImgGridImg} alt="imgGrid" />
+        <img className="horizontal" src={horizontal} alt="imgGrid" />
+        <div className="background-of-Img"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ImgGrid;
