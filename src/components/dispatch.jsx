4e5bcb95e924b9988ac62e11b32096ea100a48f1
sumitshinde-84 <shindesumit217@gmail.com>
2023-06-05 15:18:09 +0530

    add dispatch component

---
+++ src/components/dispatch.jsx
@@ -0,0 +1,36 @@
+import "./style/dispatch.css"
+const Dispatch = ({ addressFormData, getAddressFormData }) => {
+    const changeHandler = () => {
+      getAddressFormData(null);
+    };
+  
+    return (
+      <div className="dispatch">
+        <h1>ronex</h1>
+        <div className="contact-deliver-info-box">
+          <div className="contact">
+            <p className="feild-name">Contact</p>
+            <p className="feild-data">{addressFormData.phone}</p>
+            <p onClick={changeHandler} className="change-link">
+              change
+            </p>
+          </div>
+          <div className="deliver-to">
+            <p className="feild-name">Deliver to</p>
+            <p className="feild-data">{`${addressFormData.appartment}, ${addressFormData.city},${addressFormData.country}`}</p>
+            <p onClick={changeHandler} className="change-link">
+              change
+            </p>
+          </div>
+        </div>
+        <h3 className="dispatch">Dispatch</h3>
+        <div className="dispatch-main">
+            <p className="dispatch-type">Standard <span>Free</span></p>
+        </div>
+        <button>Demo pay</button>
+      </div>
+    );
+  };
+  
+  export default Dispatch;
+  
