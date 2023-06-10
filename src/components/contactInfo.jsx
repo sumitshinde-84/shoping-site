f6b13470b0c6af4c8c58f62fb301ed8dbcac6930
sumitshinde-84 <shindesumit217@gmail.com>
2023-06-10 13:19:49 +0530

    add contact page

---
+++ src/components/contactInfo.jsx
@@ -0,0 +1,32 @@
+import "./style/contactInfo.css"
+
+const ContactInfo = () => {
+
+    return (
+        <div className="contact-info">
+            <h1>Your concerns, Our mails.</h1>
+            <div className="details">
+                <div className="first">
+                    <p>Nexenic AG
+                        Klosterstrasse 34
+                        8406 Winterthur</p>
+                        <p className="contact-email">shindesumit217@gmail.com</p>
+                </div>
+                <div className="second">
+                    <p>For questions about orders:</p>
+                    <p className="contact-email">ronels800@gmail.com</p>
+                </div>
+                <div className="third">
+                    <p>For questions about orders:</p>
+                    <p className="contact-email">ronels800@gmail.com</p>
+                </div>
+            </div>
+        </div>
+
+
+    )
+
+
+}
+
+export default ContactInfo
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react"
+import ContactInfo from "../components/contactInfo"
+import Footer from "../components/footer"
+import "./style/contact.css"
 const Contact = () => {
 
 
+   
+
     return (
         <div className="Contact">
-
-
+        <ContactInfo />
+        <Footer/>
         </div>
     )
 }
